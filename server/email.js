f2fb861455c4d6bb5816b8075d6fd2cd37c76b75
emiliotelevision@gmail.com <emiliotelevision@gmail.com>
2013-11-21 22:15:03 -0500

    second commit

---
+++ server/email.js
@@ -0,0 +1,18 @@
+
+// In your server code: define a method that the client can call
+Meteor.methods({
+  emailMe: function (info) {
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+	console.log('emailing', info);
+
+    Email.send({
+      to: 'emiliotelevision@gmail.com',
+      from: info.email,
+      subject: info.name + ' from ' + info.city,
+      text: info.message
+    });
+  }
+});
